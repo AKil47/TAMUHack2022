63ac137e02ed0caf8154dd324171f0e02bf48602
Nicholas Kasman <nck.kasman@gmail.com>
2022-01-30 03:52:02 -0600

    multi-graphs

--- frontend/live_feed/graph.js
+++ frontend/live_feed/graph.js
@@ -1,53 +1,70 @@
 // import { Chart } from "/chart.js"
 import { get_call_data } from "/firebase.js"
 
-let myChart = document.getElementById('myChart').getContext('2d');
-
 const raw = await get_call_data('call_1');
 
-const emotions = [];
-
-for (let e of Object.keys(raw)) {
-    if (e != "timestamps") {
-        emotions.push(e);
-    }
+const donuts = {
+    "donut_1": {"color": "#5b2574", "emotion": "sad"},
+    "donut_2": {"color": "#a73c78", "emotion": "bored"}
 }
 
-const data = {
-    labels: emotions,
-    datasets: [
-        {
-            label: 'Emotions',
-            data: getEmotions(raw, emotions),
-            backgroundColor: ['Red', 'Pink', 'Violet']
-        },
-    ]
-}
+let donut_id = 'donut_1';
 
-const config = {
+let donut_1 = document.getElementById('donut_1').getContext('2d');
+let donut = new Chart(donut_1, {
     type: 'doughnut',
-    data: data,
-    options: {
-      responsive: true,
-      plugins: {
-        legend: {
-          position: 'top',
-        },
-        title: {
-          display: true,
-          text: 'we are donuts bum bum bum bum bum bum bum bum bum'
-        }
-      },
+    data: {
+        labels: [donuts[donut_id].emotion],
+        datasets: [
+            {
+                label: donuts[donut_id].emotion,
+                data: [Object.values(raw[donuts[donut_id].emotion])[Object.values(raw[donuts[donut_id].emotion]).length - 1], (100 - Object.values(raw[donuts[donut_id].emotion])[Object.values(raw[donuts[donut_id].emotion]).length - 1])],
+                backgroundColor: [donuts[donut_id].color, 'white']
+            }
+        ]
     },
-  };
-
-let bigboyDonut = new Chart(myChart, config);
-
-function getEmotions(data, emotions) {
-    const vals = [];
-    for (let emotion of emotions) {
-        vals.push(Object.values(data[emotion])[Object.values(data[emotion]).length - 1]);
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'we are donuts bum bum bum bum bum bum bum bum bum'
+            }
+        },
+        events: ['mouseout']
     }
+});
 
-    return vals;
-}
+let donut_2 = document.getElementById('donut_2').getContext('2d');
+donut_id = 'donut_2';
+donut = new Chart(donut_2, {
+    type: 'doughnut',
+    data: {
+        labels: [donuts[donut_id].emotion],
+        datasets: [
+            {
+                label: donuts[donut_id].emotion,
+                data: [Object.values(raw[donuts[donut_id].emotion])[Object.values(raw[donuts[donut_id].emotion]).length - 1], (100 - Object.values(raw[donuts[donut_id].emotion])[Object.values(raw[donuts[donut_id].emotion]).length - 1])],
+                backgroundColor: [donuts[donut_id].color, 'white']
+            }
+        ]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'we are donuts bum bum bum bum bum bum bum bum bum'
+            }
+        },
+        events: ['mouseout']
+    }
+});
